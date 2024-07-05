a56657ecffb301fde86cc92ccde809459a7ad827
Chris <nzeanoruec@gmail.com>
2024-07-05 03:22:33 -0400

    ssss

--- db/config.js
+++ db/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-	.connect(`mongodb+srv://ChrisNze:Chris08037980751@chriscluster.k0ihdyw.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=ChrisCluster`)
+	.connect(`mongodb://mongo:bLGpZqzoGyAyiVVUYNPLnMKmZPzShroI@monorail.proxy.rlwy.net:29255`)
 	.then(() => console.log("Connected to Monogodb................"))
 	.catch((err) => console.error("Could not connect to Monogodb............." + err));
 // mongodb+srv://ChrisNze:Chris08037980751@chriscluster.k0ihdyw.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=ChrisCluster
