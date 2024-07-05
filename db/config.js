aa8ae912b33cc334fbbab89843bc257bdf5cb206
Chris <nzeanoruec@gmail.com>
2024-07-05 00:00:08 -0400

    Added Mongo Env Url

--- db/config.js
+++ db/config.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-	.connect("mongodb://127.0.0.1:27017/e-commerce")
+	.connect(MONGO_URI)
 	.then(() => console.log("Connected to Monogodb................"))
-	.catch((err) => console.error("Could not connect to Monogodb............."));
+	.catch((err) => console.error("Could not connect to Monogodb............." + err));
+// mongodb+srv://ChrisNze:Chris08037980751@chriscluster.k0ihdyw.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=ChrisCluster
