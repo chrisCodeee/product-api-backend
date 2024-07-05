316c00a657890dc73813e7ee68269ce67f3164a8
Chris <nzeanoruec@gmail.com>
2024-07-05 01:04:18 -0400

    Modified the build

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const users = require("./routes/users");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.use(express.json());
 app.use(cors()); //Used to give access to our frontend to get data from our backend. Note that this is not advisable as our api is open to any website to access
